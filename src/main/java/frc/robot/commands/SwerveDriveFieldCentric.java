f2d927561064ad69ba62854316799943ad0051b9
sepandar <sepandar24@gmail.com>
2024-08-01 19:14:05 -0700

    imported

--- src/main/java/frc/robot/commands/SwerveDriveFieldCentric.java
+++ src/main/java/frc/robot/commands/SwerveDriveFieldCentric.java
@@ -5,10 +5,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 
-public class SwerveDriveFieldCentric extends CommandBase {
+public class SwerveDriveFieldCentric extends Command {
   private XboxController joystick;
   private DriveBaseSubsystem driveBaseSubsystem;
 
--- src/main/java/frc/robot/commands/TranslateDistance.java
+++ src/main/java/frc/robot/commands/TranslateDistance.java
@@ -5,10 +5,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drive.DriveBaseSubsystem;
 
-public class TranslateDistance extends CommandBase {
+public class TranslateDistance extends Command {
   private final DriveBaseSubsystem driveBaseSubsystem;
   private final double xVelocity;
   private final double yVelocity;
