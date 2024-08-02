8833078dcb522ad1b90dba9cc781df1c28c72317
sepandar <sepandar24@gmail.com>
2024-08-01 20:12:17 -0700

    setup

---
+++ src/main/java/frc/robot/subsystems/drive/ShooterSubsystem.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+  // Things the subsystem will do:
+  // - spin top and bottom shooter motors(Falcons)
+  // - Beambreak
+  // - The shooter will be at a fixed angle
+  public ShooterSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {}
+}
