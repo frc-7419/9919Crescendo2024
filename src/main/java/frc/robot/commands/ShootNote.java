120ceebeef1aec900faaa282605a907cb8aae721
Bachthoven <bachtoven.rules@gmail.com>
2024-10-21 13:08:14 -0700

    Update ShootNote.java

--- src/main/java/frc/robot/commands/ShootNote.java
+++ src/main/java/frc/robot/commands/ShootNote.java
@@ -4,8 +4,6 @@
 
 package frc.robot.commands;
 
-package frc.robot.commands;
-
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
