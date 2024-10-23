57ed33b4b8c8bd03882dab22bd26a5a4f6c6ef63
mqxs21 <maxshi239@gmail.com>
2024-10-22 18:52:02 -0700

    used LimelightResults method instead of pose estimate method
    Tested in ris, poseestimate kept returning null for some reason.

--- src/main/java/frc/robot/subsystems/LimelightRangeChecker.java
+++ src/main/java/frc/robot/subsystems/LimelightRangeChecker.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,37 +11,50 @@ import frc.robot.LimelightHelpers.RawFiducial;
 public class LimelightRangeChecker extends SubsystemBase {
     private final int blueSpeakerFiducialID = 7;
     private final int redSpeakerFiducialID = 4;
+    private final int TARGET_HEIGHT = 3; //3 feet(need to test)
+    private final int LIMELIGHT_HEIGHT = 1; //1 feet off the ground(need to test as well)
 
     public LimelightRangeChecker() {}
 
+    public double getDistance(LimelightHelpers.LimelightTarget_Fiducial fiducial) {
+        Rotation2d angleToGoal = Rotation2d.fromDegrees(fiducial.ty);//could be -fiducial.ty 
+        
+        double distance = (TARGET_HEIGHT - LIMELIGHT_HEIGHT) / angleToGoal.getTan(); // math formula that i found on docmentation 
+        SmartDashboard.putNumber("Distance to Fiducial " + fiducial.fiducialID, distance);
+        return distance;
+    }
+    
 
     public boolean speakerFiducialInRange(int targetRange) {
-        LimelightHelpers.PoseEstimate poseEstimate; //making a PoseEstimate, which is what limeleight helpers uses for pose stuff
-        if (DriverStation.getAlliance().toString().equalsIgnoreCase("blue")) { // getting pose estimate based on alliance color
-            poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight");
-        } else {
-            poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiRed("limelight");
-        }
-        if (poseEstimate.tagCount <= 0) { // making sure there are fiducials detected
-            SmartDashboard.putString("Currently", " no fiducials detected"); // for debugging purposes
+        LimelightHelpers.LimelightResults llresults = LimelightHelpers.getLatestResults("limelight");
+        
+        if (llresults == null || llresults.targets_Fiducials == null) {
+            SmartDashboard.putString("Currently", "No Limelight results found");
+            System.out.println("NOTHING FOUND :-(");
             return false;
-        }
-        //limelight helpers has this weird system where theres the pose estimate, and then in them theres many fiducials(as an array of rawfiducials), and then for each one you can get the id and distance(and a bunch of otehr very helpful stuff)
-        for (RawFiducial fiducial : poseEstimate.rawFiducials) {
-            if ((DriverStation.getAlliance().toString().equalsIgnoreCase("blue") && fiducial.id == blueSpeakerFiducialID) ||
-                (DriverStation.getAlliance().toString().equalsIgnoreCase("red")&& fiducial.id == redSpeakerFiducialID)) {
-                SmartDashboard.putString("Currently", " speaker fiducial detected at distance(not in range): " + fiducial.distToCamera); // for debugging
-                if (fiducial.distToCamera <= targetRange) { //make sure its in range
-                    SmartDashboard.putString("Currently", " speaker fiducial is in range"); // for debugging
-                    return true;
+        } else {
+            System.out.println("NOT NULL :-))");
+            
+            // Iterate through fiducials to find speaker fiducial
+            for (LimelightHelpers.LimelightTarget_Fiducial fiducial : llresults.targets_Fiducials) {
+                double distance = getDistance(fiducial);
+                if ((DriverStation.getAlliance().toString().equalsIgnoreCase("blue") && fiducial.fiducialID == blueSpeakerFiducialID) ||
+                    (DriverStation.getAlliance().toString().equalsIgnoreCase("red") && fiducial.fiducialID == redSpeakerFiducialID)) {
+    
+                    SmartDashboard.putString("Currently", "Speaker fiducial detected at distance: " + distance);
+                    if (distance <= targetRange) {
+                        SmartDashboard.putString("Currently", "Speaker fiducial is in range");
+                        return true;
+                    }
+                } else {
+                    
+                    SmartDashboard.putString("Currently", "No speaker fiducial detected, other fiducials at distance: " + distance);
                 }
-                
-            }else{
-                SmartDashboard.putString("Currently", " no fiducial with speaker id detected, other fiducials detected at range: "+fiducial.distToCamera);
             }
         }
-        return false; 
+        return false;
     }
+    
 
     @Override
     public void periodic() {
