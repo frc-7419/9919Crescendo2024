aa2846e1e4f43e1cb1b84e8ab62c4a2f1b3fe61d
Bachthoven <bachtoven.rules@gmail.com>
2024-10-23 10:28:11 -0700

    Fixed indentation

--- src/main/java/frc/robot/commands/RunShooterCommand.java
+++ src/main/java/frc/robot/commands/RunShooterCommand.java
@@ -9,35 +9,35 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
 public class RunShooterCommand extends Command {
-    private final ShooterSubsystem shooterSubsystem;
-    private final double power;
+  private final ShooterSubsystem shooterSubsystem;
+  private final double power;
 
-    public RunShooterCommand(ShooterSubsystem shooterSubsystem, double power) {
-        this.shooterSubsystem = shooterSubsystem;
-        this.power = power;
-        addRequirements(shooterSubsystem);
-    }
+  public RunShooterCommand(ShooterSubsystem shooterSubsystem, double power) {
+    this.shooterSubsystem = shooterSubsystem;
+    this.power = power;
+    addRequirements(shooterSubsystem);
+  }
 
-    @Override
-    public void initialize() {
-      shooterSubsystem.coast(); // Set the motors to coast mode
-        shooterSubsystem.run(power, power); // Assuming both motors run at the same power
-    }
+  @Override
+  public void initialize() {
+    shooterSubsystem.coast(); // Set the motors to coast mode
+    shooterSubsystem.run(power, power); // Assuming both motors run at the same power
+  }
 
-    @Override
-    public void execute() {
-      
+  @Override
+  public void execute() {
+    
+  }
+  @Override
+  public void end(boolean interrupted) {
+    if (!DriverStation.isAutonomous()) {
+      shooterSubsystem.stop();
+      shooterSubsystem.brake(); // Set the motors to brake mode
     }
-    @Override
-    public void end(boolean interrupted) {
-      if (!DriverStation.isAutonomous()) {
-        shooterSubsystem.stop();
-        shooterSubsystem.brake(); // Set the motors to brake mode
-      }
-    }
+  }
 
-    @Override
-    public boolean isFinished() {
-        return false; // This command runs until interrupted
-    }
+  @Override
+  public boolean isFinished() {
+    return false; // This command runs until interrupted
+  }
 }
