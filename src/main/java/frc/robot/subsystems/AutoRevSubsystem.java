456da741504e2b398e1a517d97a55179e2c251e6
Amrit-s-RIS <therealamritsingh@gmail.com>
2024-10-26 23:48:52 -0700

    Rough draft of firing when revved code

--- src/main/java/frc/robot/subsystems/AutoRevSubsystem.java
+++ src/main/java/frc/robot/subsystems/AutoRevSubsystem.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
 import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
+import java.util.concurrent.TimeUnit;
 
 public class AutoRevSubsystem extends SubsystemBase {
   private final ShooterSubsystem shooterSubsystem;
@@ -19,10 +20,12 @@ public class AutoRevSubsystem extends SubsystemBase {
   private boolean inRange;
   private boolean toggleAutoRev;
   private final double lineXValue;
+  private double revingTime;
   public AutoRevSubsystem(ShooterSubsystem shooterSubsystem, CommandSwerveDrivetrain commandSwerveDrivetrain, CommandXboxController commandXboxController) {
     this.shooterSubsystem = shooterSubsystem;
     this.commandXboxController = commandXboxController;
     this.commandSwerveDrivetrain = commandSwerveDrivetrain;
+    this.revingTime = 0;
     lineXValue = 3; //change after getting actual start line x
     inRange = true;
     toggleAutoRev = true;
@@ -34,6 +37,15 @@ public class AutoRevSubsystem extends SubsystemBase {
         inRange = true;
         if (toggleAutoRev) {
           shooterSubsystem.run(Constants.ShooterConstants.topShooterRPM,Constants.ShooterConstants.bottomShooterRPM);
+          revingTime += 1;
+          TimeUnit.SECONDS.sleep(0);
+          revingTime += 1;
+          TimeUnit.SECONDS.sleep(0);
+          revingTime += 1;
+          //the value of reving time is the amount of seconds since the shooter has started revving (someone please look into these syntax errors i need to sleep)
+
+
+
         }
     }else{
         inRange = false;
@@ -48,5 +60,17 @@ public class AutoRevSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     SmartDashboard.putBoolean("In Shoot Distance", inRange);
+  }
+  public double getRevingTime(){
+    return revingTime;
+
+  }
+  public boolean getInRange(){
+    Pose2d currentPos = commandSwerveDrivetrain.getPose2d();
+    double currentX = currentPos.getX();
+    if(currentX <= lineXValue){
+      return true;
+
+     }
   }
 }
