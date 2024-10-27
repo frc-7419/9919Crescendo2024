33aececdc8e3f1b13f95119fa6a9f79256c16ef6
MostOriginalIGN <astrogamer543@gmail.com>
2024-10-26 18:32:53 -0700

    wpilog

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ package frc.robot;
 
 import com.ctre.phoenix6.SignalLogger;
 
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -21,7 +22,8 @@ public class Robot extends TimedRobot {
     @Override
     public void robotInit() {
         robotContainer = new RobotContainer();
-        //SignalLogger.start();
+        // SignalLogger.start();
+        DataLogManager.start();
     }
 
     @Override
