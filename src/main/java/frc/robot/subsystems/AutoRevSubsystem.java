a1c44f0431f03e1c9ac716840da1871c779bd261
Ary <ary10kum@gmail.com>
2024-10-27 17:11:49 -0700

    Edited AutoRevSubsystem syntax and removed boolean conditional

--- src/main/java/frc/robot/subsystems/AutoRevSubsystem.java
+++ src/main/java/frc/robot/subsystems/AutoRevSubsystem.java
@@ -21,56 +21,51 @@ public class AutoRevSubsystem extends SubsystemBase {
   private boolean toggleAutoRev;
   private final double lineXValue;
   private double revingTime;
+
   public AutoRevSubsystem(ShooterSubsystem shooterSubsystem, CommandSwerveDrivetrain commandSwerveDrivetrain, CommandXboxController commandXboxController) {
     this.shooterSubsystem = shooterSubsystem;
     this.commandXboxController = commandXboxController;
     this.commandSwerveDrivetrain = commandSwerveDrivetrain;
     this.revingTime = 0;
-    lineXValue = 3; //change after getting actual start line x
+    lineXValue = 3; // change after getting actual start line x
     inRange = true;
     toggleAutoRev = true;
   }
-  public void revWhenInRange() throws InterruptedException{
-     Pose2d currentPos = commandSwerveDrivetrain.getPose2d();
+
+  public void revWhenInRange() throws InterruptedException {
+    Pose2d currentPos = commandSwerveDrivetrain.getPose2d();
     double currentX = currentPos.getX();
-    if(currentX <= lineXValue){
-        inRange = true;
-        if (toggleAutoRev) {
-          shooterSubsystem.run(Constants.ShooterConstants.topShooterRPM,Constants.ShooterConstants.bottomShooterRPM);
-          TimeUnit.SECONDS.sleep(4); //TODO: fix this syntax error and figure out the actual amount of time it takes for shooter to rev
-          revingTime += 4;
-          //the value of reving time is the amount of seconds since the shooter has started revving (someone please look into these syntax errors i need to sleep)
-
-
-
-        }
-    }else{
-        inRange = false;
+    if (currentX <= lineXValue) {
+      inRange = true;
+      if (toggleAutoRev) {
+        shooterSubsystem.run(Constants.ShooterConstants.topShooterRPM, Constants.ShooterConstants.bottomShooterRPM);
+        // Wait for the shooter to rev up; the time may need to be adjusted based on testing
+        long revTime = 4; // seconds
+        TimeUnit.SECONDS.sleep(revTime);
+        revingTime += revTime;
+      }
+    } else {
+      inRange = false;
     }
 
-    if (commandXboxController.getLeftTriggerAxis()>=0.1 && commandXboxController.getRightTriggerAxis()>=0.1) {
+    if (commandXboxController.getLeftTriggerAxis() >= 0.1 && commandXboxController.getRightTriggerAxis() >= 0.1) {
       toggleAutoRev = !toggleAutoRev;
       if (!toggleAutoRev) shooterSubsystem.brake();
     }
-    
   }
+
   @Override
   public void periodic() {
     SmartDashboard.putBoolean("In Shoot Distance", inRange);
   }
-  public double getRevingTime(){
-    return revingTime;
 
+  public double getRevingTime() {
+    return revingTime;
   }
-  public boolean getInRange(){
+
+  public boolean getInRange() {
     Pose2d currentPos = commandSwerveDrivetrain.getPose2d();
     double currentX = currentPos.getX();
-    if(currentX <= lineXValue){
-      return true;
-
-     }else{
-      return false;
-     }
-  
+    return currentX <= lineXValue;
   }
 }
